9d8327ff9f31e0ed8968c834d558064e520db94b
sidapon0903 <sidaphon.pr@rmuti.ac.th>
2025-01-21 01:58:17 +0700

    แก้ไขล่าสุด

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -7,6 +7,5 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
-    provideHttpClient()
-  ]
+    provideHttpClient() ]
 };
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,29 +1,39 @@
 import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, FormRecord, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup,FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PasswordValidator } from '../_validator/password.validator';
 import { PasswordMatchValidator } from '../_validator/password.match.validator';
 import { CommonModule } from '@angular/common';
-
 import {MatInputModule} from '@angular/material/input';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule,FormsModule,CommonModule,MatInputModule,MatFormFieldModule,MatButtonModule,FormsModule],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule,MatInputModule,MatFormFieldModule,MatButtonModule,FormsModule,MatRadioModule,MatNativeDateModule,MatRadioModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
- 
-})
+  providers:[provideNativeDateAdapter()]
+ })
+
 export class LoginComponent {
   mode : 'login' | 'register'= 'login'
   form:FormGroup
 
+  private readonly _currentYear = new Date().getFullYear()
+  readonly minDate = new Date(this._currentYear - 70,0,1)
+  readonly maxDate = new Date(this._currentYear - 18,11,31)
+  readonly starDate = new Date(this._currentYear - 18,0,1)
+
   errorMessages = {
     username :signal(''),
     password :signal(''),
-     dispaly_name:signal(''),
-    confirm_password:signal(''),
+    dispaly_name:signal(''),
+    confirm_password:signal('')
     
   }
 
@@ -39,7 +49,7 @@ toggleMode(){
 }
   updaForm() {
    if (this.mode === 'register'){
-this.form.addControl('confirmpassword',new FormControl(null,Validators.required))
+this.form.addControl('confirm_password',new FormControl(null,[Validators.required]))
 this.form.addValidators(PasswordMatchValidator('password','confim_password'))
 
 this.form.addControl('display_name',new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(8)]))
@@ -47,7 +57,7 @@ this.form.addControl('date_of_birth',new FormControl(null,Validators.required))
 this.form.addControl('gander',new FormControl(null,Validators.required))
 this.form.addControl('looking_for',new FormControl(null,Validators.required))
    }else{
-    this.form.removeControl('confirmpasseord')
+    this.form.removeControl('confirm_passeord')
     this.form.removeValidators(PasswordMatchValidator ('password','confim_password'))
     this.form.removeControl('diaplay_name')
     this.form.removeControl('date_of_birth')
@@ -57,6 +67,8 @@ this.form.addControl('looking_for',new FormControl(null,Validators.required))
    }
 }
 onSudmit(){
+  if (this.form.invalid)return
+  console.log(this.form.value)
 
 }
 updateErrorMassage(ctrlName:string){
@@ -75,27 +87,24 @@ updateErrorMassage(ctrlName:string){
     break
     case'password':
     if (control.hasError('required'))
-      this.errorMessages.username.set('required')
+      this.errorMessages.password.set('required')
     else if (control.hasError('invaLidMinLength'))
-      this.errorMessages.username.set('must br at last 8 characters long')
+      this.errorMessages.password.set('must br at last 8 characters long')
     else if (control.hasError('invaLidMaxLength'))
-      this.errorMessages.username.set('must br at last 16 characters long') 
+      this.errorMessages.password.set('must br at last 16 characters long') 
     else if (control.hasError('invaLidLowerCase'))
-        this.errorMessages.username.set('must contain minimum of 1 lower case letter')
+        this.errorMessages.password.set('must contain minimum of 1 lower case letter')
     else if (control.hasError('invaLidUpperCase'))
-      this.errorMessages.username.set('must contain minimum of 1 lower case letter')
+      this.errorMessages.password.set('must contain minimum of 1 lower case letter')
     else if (control.hasError('invaLidNumeric'))
-      this.errorMessages.username.set('must contain minimum of 1 numeric character')
+      this.errorMessages.password.set('must contain minimum of 1 numeric character')
     else if (control.hasError('invaLidSpercialChar'))
-      this.errorMessages.username.set('must contain minimum of 1 special character')
+      this.errorMessages.password.set('must contain minimum of 1 special character')
     break
-    this.errorMessages.username.set('')
+    this.errorMessages.password.set('')
     break
 
-    case'display_name':
-   break
-
-    case 'confirm_password':
+      case 'confirm_password':
         if (control.hasError('required'))
           this.errorMessages.confirm_password.set('required')
         else if (control.hasError('misMatch'))
@@ -103,9 +112,18 @@ updateErrorMassage(ctrlName:string){
         else
           this.errorMessages.confirm_password.set('')
         break
-  }
 
-
-  }
+      case 'display_name':
+        if (control.hasError('required'))
+          this.errorMessages.dispaly_name.set('required')
+        else if (control.hasError('minlength'))
+          this.errorMessages.dispaly_name.set('must be at least 3 characters long')
+        else if (control.hasError('maxlength'))
+          this.errorMessages.dispaly_name.set('must be at least 8 characters long')
+        else
+          this.errorMessages.dispaly_name.set('')
+        break
+}
+}
 }
 
